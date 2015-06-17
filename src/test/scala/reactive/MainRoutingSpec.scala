069d1354f5bf290b2427522ff6cea950a19b2678
Pim Verkerk <pimverkerk@hotmail.com>
2015-06-17 16:08:21 +0200

    Some sort of websocket test working

--- src/test/scala/reactive/MainRoutingSpec.scala
+++ src/test/scala/reactive/MainRoutingSpec.scala
@@ -2,10 +2,8 @@ package reactive
 
 import scala.concurrent.duration.DurationInt
 import scala.language.postfixOps
-
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
-
 import akka.http.impl.engine.ws.InternalCustomHeader
 import akka.http.impl.engine.ws.InternalCustomHeader
 import akka.http.scaladsl.model.HttpRequest
@@ -14,10 +12,16 @@ import akka.http.scaladsl.model.StatusCodes.OK
 import akka.http.scaladsl.model.ws.Message
 import akka.http.scaladsl.model.ws.UpgradeToWebsocket
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.http.scaladsl.model.headers.{ UpgradeProtocol, Upgrade }
 import akka.pattern.ask
 import akka.stream.ActorFlowMaterializer
 import akka.stream.FlowMaterializer
 import akka.util.Timeout
+import akka.http.impl.engine.ws.InternalCustomHeader
+import akka.http.scaladsl.model.HttpResponse
+import akka.stream.scaladsl.Flow
+import akka.http.scaladsl.model.headers.CustomHeader
+import akka.http.impl.engine.ws.InternalCustomHeader
 
 class MainRoutingSpec extends FlatSpec with Matchers with ScalatestRouteTest {
   "Main" should "respond with index on /" in {
@@ -32,8 +36,26 @@ class MainRoutingSpec extends FlatSpec with Matchers with ScalatestRouteTest {
   "the handleWebsocketMessages directive" should "handle websocket requests" in {
     implicit val timeout = Timeout(1000 millis)
     implicit val materializer = ActorFlowMaterializer()
-    Get("/") ~> Main.mainFlow ~> check {
+    Get("/") ~> Upgrade(List(UpgradeProtocol("websocket"))) ~> emulateHttpCore ~> Main.mainFlow ~> check {
       status shouldEqual StatusCodes.SwitchingProtocols
     }
   }
+
+  /** Only checks for upgrade header and then adds UpgradeToWebsocket mock header */
+  def emulateHttpCore(req: HttpRequest): HttpRequest =
+    req.header[Upgrade] match {
+      case Some(upgrade) if upgrade.hasWebsocket ⇒ req.copy(headers = req.headers :+ upgradeToWebsocketHeaderMock)
+      case _                                     ⇒ req
+    }
+  def upgradeToWebsocketHeaderMock: UpgradeToWebsocket =
+    new CustomHeader() with UpgradeToWebsocket {
+      override def requestedProtocols = Nil
+override def suppressRendering: Boolean = true
+      override def name = ""
+      override def value = "UpgradeToWebsocketMock"
+      
+
+      override def handleMessages(handlerFlow: Flow[Message, Message, Any], subprotocol: Option[String])(implicit mat: FlowMaterializer): HttpResponse =
+        HttpResponse(StatusCodes.SwitchingProtocols)
+    }
 }
